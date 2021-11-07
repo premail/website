f97be26bd4f76e5e72de03d9aaafb3ec387d9400
Ivan Boothe <git@rootwork.org>
2021-11-07 14:11:56 -0800

    Update repo name
    Signed-off-by: Ivan Boothe <git@rootwork.org>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
   organizationName: 'premail', // Usually your GitHub org/user name.
-  projectName: 'premail', // Usually your repo name.
+  projectName: 'website', // Usually your repo name.
   trailingSlash: true,
 
   presets: [
